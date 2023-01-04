aae682dd2b256e472489ccbd0d4368b1839aa2ba
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-04 13:12:43 +0000

    create password length prompt

--- scripts/script.js
+++ scripts/script.js
@@ -95,14 +95,32 @@ var randomPasswordEl = document.querySelector("#random-password");
 
 // create a function that is executed when the 'generate password button' is clicked
 function generatePassword() {
-    
+  var passwordLength = 0;
+
+  // prompt the user for password length (>= 10 || <= 64)
+  var lengthPrompt = prompt(
+    "Choose a password length between 10 and 64 characters"
+  );
+
+  // create a function that converts the string to a number and store it in the 'passwordLength' variable
+  function changeToNum() {
+    passwordLength = parseInt(lengthPrompt);
+  }
+
+  changeToNum();
+
+  // check whether user input fits the criteria
+  while (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 64) {
+    lengthPrompt = prompt(
+      "Wrong Input! Please try again! Pick a length between 10 and 64 characters:"
+    );
+    changeToNum();
+  }
+  // tell the user how many characters they have picked
+  alert(`Your password will have ${passwordLength} characters`);
+  // ask the user which character type to include (lowercase, uppercase, numeric and/or special characters
+  // - make the user pick at least one)
+  // concatenate the chosen character types and store them in an array
+  // create a function that picks a random item from an array
+  // for the length of the password, pick the items from the character array at random
 }
-// prompt the user for password length (>= 10 || <= 64)
-// create a function that converts the string to a number
-// check whether user input fits the criteria
-// tell the user how many characters they have picked
-// ask the user which character type to include (lowercase, uppercase, numeric and/or special characters
-// - make the user pick at least one)
-// concatenate the chosen character types and store them in an array
-// create a function that picks a random item from an array
-// for the length of the password, pick the items from the character array at random
