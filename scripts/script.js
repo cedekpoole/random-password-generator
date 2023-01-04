8e6e9679db93b965f297a4d34d6c369652bfa5f8
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-01-04 13:07:34 +0000

    add code so function is executed when button is clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,105 +1,108 @@
 var upperCaseChar = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  
-  var lowerCaseChar = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  
-  var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  
-  var specialChar = [
-    "~",
-    "`",
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "_",
-    "-",
-    "+",
-    "=",
-    "{",
-    "[",
-    "}",
-    "]",
-    ",",
-    "|",
-    ":",
-    ";",
-    "<",
-    ">",
-    ".",
-    "?",
-    "/",
-  ];
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-  // create a function that is executed when the 'generate password button' is clicked
-  // prompt the user for password length (>= 10 || <= 64)
-  // create a function that converts the string to a number
-  // check whether user input fits the criteria
-  // tell the user how many characters they have picked
-  // ask the user which character type to include (lowercase, uppercase, numeric and/or special characters
-  // - make the user pick at least one)
-  // concatenate the chosen character types and store them in an array 
-  // create a function that picks a random item from an array
-  // for the length of the password, pick the items from the character array at random 
+var lowerCaseChar = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
+var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+var specialChar = [
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
+// when button is pressed, show content in paragraph element
+var randomPasswordEl = document.querySelector("#random-password");
+
+// create a function that is executed when the 'generate password button' is clicked
+function generatePassword() {
+    
+}
+// prompt the user for password length (>= 10 || <= 64)
+// create a function that converts the string to a number
+// check whether user input fits the criteria
+// tell the user how many characters they have picked
+// ask the user which character type to include (lowercase, uppercase, numeric and/or special characters
+// - make the user pick at least one)
+// concatenate the chosen character types and store them in an array
+// create a function that picks a random item from an array
+// for the length of the password, pick the items from the character array at random
